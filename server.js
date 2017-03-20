705a3fdcf3815400c7dd272bec92c6f39fc1b517
Parker Kirby <jkirby@boomtownroi.com>
2017-03-19 22:26:12 -0400

    different server code

--- server.js
+++ server.js
@@ -1,35 +1,16 @@
-var http = require("http"),
-    url = require("url"),
-    path = require("path"),
-    fs = require("fs")
-    port = process.argv[2] || 8888;
+var express = require('express');
+var app = express();
 
-http.createServer(function(request, response) {
+var port = process.env.PORT || 8080;
 
-  var uri = url.parse(request.url).pathname
-    , filename = path.join(process.cwd(), uri);
-  
-  path.exists(filename, function(exists) {
-    if(!exists) {
-      response.writeHead(404, {"Content-Type": "text/plain"});
-      response.write("404 Not Found\n");
-      response.end();
-      return;
-    }
+app.set('view engine', 'ejs');
 
-    if (fs.statSync(filename).isDirectory()) filename += '/index.html';
+app.use(express.static(__dirname + '/'));
 
-    fs.readFile(filename, "binary", function(err, file) {
-      if(err) {        
-        response.writeHead(500, {"Content-Type": "text/plain"});
-        response.write(err + "\n");
-        response.end();
-        return;
-      }
+app.get('/', function(req, res) {
+    res.render('index');
+});
 
-      response.writeHead(200);
-      response.write(file, "binary");
-      response.end();
-    });
-  });
-}).listen(parseInt(port, 10));
+app.listen(port, function() {
+    console.log('App is running on http://localhost:' + port);
+});
