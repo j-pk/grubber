bebff8843001a66d43e61251e1eb0cc91c2bec47
Parker Kirby <jkirby@boomtownroi.com>
2017-03-19 22:18:29 -0400

    change in server code

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var port = process.env.PORT || 3000;
-var app = express.createServer();
+var express = require('express');
+var app = express();
+//Middleware
+app.listen();
 
 app.get('/', function(request, response) {
     response.sendfile(__dirname + '/index.html');
