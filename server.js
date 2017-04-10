eddee6d7a82e50f2a2041352d0bbb9b5070f07dc
Parker Kirby <jkirby@boomtownroi.com>
2017-04-09 21:37:01 -0400

    added CORS support #14

--- server.js
+++ server.js
@@ -3,13 +3,8 @@ var cors = require('cors')
 var app = express()
 
 app.use(function(req, res, next) {
-  var allowedOrigins = ['http://127.0.0.1:8020', 'http://localhost:8020', 'http://127.0.0.1:9000', 'http://localhost:9000'];
-  var origin = req.headers.origin;
-  if(allowedOrigins.indexOf(origin) > -1){
-       res.setHeader('Access-Control-Allow-Origin', origin);
-  }
-  //res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:8020');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, OPTIONS');
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.header('Access-Control-Allow-Credentials', true);
   return next();
